adba0b34d66fba7be16fe59bd7b9f070250ae5bf
sensui <sensuijunior@hotmail.com>
2020-08-18 01:32:08 -0300

    project finished

--- src/helpers/getGifs.jsx
+++ src/helpers/getGifs.jsx
@@ -1,6 +1,5 @@
 
 export const getGifs = async (category) => {
-
 	const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=SCfw8skoqhhW8J44dmeAMBf7Ov4VVeej`;
 	const resp = await fetch(url);
 	const { data } = await resp.json();
