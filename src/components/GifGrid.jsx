0a5bf12e3b08754ddce39f1f17b4668fd6200af6
sensui <sensuijunior@hotmail.com>
2020-08-17 22:34:30 -0300

    custom hooks

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,37 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { GifGridItem } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs';
 
 const GifGrid = ({ category }) => {
 
 	const [images, setImages] = useState([]);
 
 	useEffect(() => {
-		getGifs();
-	}, [])
-
-	const getGifs = async () => {
-
-		const url = "https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=SCfw8skoqhhW8J44dmeAMBf7Ov4VVeej";
-		const resp = await fetch(url);
-		const { data } = await resp.json();
-
-		const gifs = data.map(img => {
-			return {
-				id: img.id,
-				title: img.title,
-				url: img.images?.downsized_medium.url
-			}
-		})
-
-		console.log(gifs);
-		setImages(gifs);
-
-	}
+		getGifs(category)
+		.then(setImages)
+	}, [category])
 
 
 	return (
-		<div>
+		<>
 			<h3> {category} </h3>
 			<div className="card-grid">
 				{
@@ -43,7 +26,7 @@ const GifGrid = ({ category }) => {
 					)))
 				}
 			</div>
-		</div>
+		</>
 	)
 }
 
